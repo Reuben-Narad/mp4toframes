10c611c7c9b8d061999711e79667a8b60434711a
Reuben-Narad <rnarad@uw.edu>
2021-07-01 11:47:49 -0700

    added frame splitting and name reformatting

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import os
+import numpy
+import cv2
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+vidcap = cv2.VideoCapture('shortclip.mp4')
+success, image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  success, image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
 
+def format(number):
+    string = re.sub("[^0-9]", "", number)
+    string = int(string)
+    if string <= 200:
+        string += 200
+    else:
+        string -= 200
+    string = str(string)
+    number_os = 3 - len(string)
+    for i in range(number_os):
+        string = '0' + string
+    return string + '.jpg'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#for file in os.listdir():
+    #os.rename(file, format(file))
